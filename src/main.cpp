611e9a3547394eea1048b28a8bb9b80eaf40cb19
pmschz <paulinasanchez177@gmail.com>
2025-03-14 20:03:07 -0500

    practice

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ int main() {
   while(input != "exit"){
     std::cout << "$ ";
     std::getline(std::cin, input);
-    std::cerr << "nonexistent: command not found" << std::endl;
     std::cerr << input << ": command not found" << std::endl;
   }
   return 0;
