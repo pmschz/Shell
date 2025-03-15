a23fedef6559b7c2b3c22cd1b92f3d850494ef88
pmschz <paulinasanchez177@gmail.com>
2025-03-14 20:00:01 -0500

    practice

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -6,10 +7,14 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
 
   std::string input;
-  std::getline(std::cin, input);
-  std::cerr << input << ": command not found" << std::endl;
-  std::cerr << input << ": command not found" << std::endl;
+
+  while(input != "exit"){
+    std::cout << "$ ";
+    std::getline(std::cin, input);
+    std::cerr << "nonexistent: command not found" << std::endl;
+    std::cerr << input << ": command not found" << std::endl;
+  }
+  return 0;
 }
