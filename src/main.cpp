deb63c9006e8b6c55a07502c66a4acfe4c142830
pmschz <paulinasanchez177@gmail.com>
2025-03-14 19:56:51 -0500

    practice

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,6 @@ int main() {
 
   std::string input;
   std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  std::cerr << input << ": command not found" << std::endl;
+  std::cerr << input << ": command not found" << std::endl;
 }
