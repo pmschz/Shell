756c5aefbb3384b97ea8d8ea2f92f91bfbf41d05
pmschz <paulinasanchez177@gmail.com>
2025-03-28 02:09:48 -0500

    single quote feature added

--- Shell/main.cpp
+++ Shell/main.cpp
@@ -1,105 +1,116 @@
 #include <iostream>
-#include <vector>
-#include <utility>
-#include <map>
-#include <filesystem>
+#include <sstream>
+#include <cstdlib>
+#include <fstream>
 #include <unistd.h>
-namespace fs = std::filesystem;
+using namespace std;
 
-using std::vector;
-using std::string;
-
-std::vector<std::string> gValid_commands = {"echo", "exit", "type", "cd"};
-bool in_vector(std::vector<std::string> vec, std::string input){
-  for (int i = 0; i < vec.size(); i++){
-    if (vec[i] == input){
-      return true;
+string getPath(string filename){
+  string pathEnv = getenv("PATH");
+  stringstream ss(pathEnv);
+  string path;
+  while(!ss.eof()){
+    getline(ss,path,':');
+    string absPath=path+"/"+filename;
+    if(filesystem::exists(absPath)){
+      return absPath;
     }
   }
-  return false;
+  return "";
 }
-std::pair<string, string> split_first(string input){
-  string first = "";
-  string second = "";
-  bool after_space = false;
-  int size = input.size();
-  for (int i = 0; i < size; i++){
-    if (input[i] == ' ' && !after_space){
-      after_space = true;
-    } else if (!after_space) first += input[i];
-    else second += input[i];
+bool execprog(string input){
+  stringstream ss(input);
+  string t;
+  vector<string> args;
+  char del=' ';
+  while(getline(ss,t,del)){
+    args.push_back(t);
   }
-  return {first, second};
-}
-vector<string> splitWith(string input, char delimiter){
-  vector<string> result;
-  string temp = "";
-  for (int i = 0; i < input.size(); i++){
-    if (input[i] == delimiter){
-      result.push_back(temp);
-      temp = "";
-    } else temp += input[i];
+  string path=getPath(args[0]);
+  if(path.empty()){
+    return false;
   }
-  result.push_back(temp);
-  return result;
-}
-int commandStatus(string command, vector<string> paths){
-  bool is_built_in = in_vector(gValid_commands, command);
-  if (is_built_in) return 1;
-  for (int i = 0; i < paths.size(); i++)
-    if (fs::exists(paths[i] + "/" + command))
-      return 2+i;
-  return 0;
-}
-void echo(string input, vector<string> &paths){ std::cout << input << std::endl;}
-void type(string input, vector<string> &paths){
-  int status = commandStatus(input, paths);
-  if (status == 1)
-    std::cout << input << " is a shell builtin" << std::endl;
-  else if (status >= 2)
-    std::cout << input << " is " << paths[status-2]+"/"+input << std::endl;
-  else std::cout << input << " not found" << std::endl;
-}
-void pwd(string input, vector<string> &paths){
-  std::cout << fs::current_path() << std::endl;
-}
-void cd(string input, vector<string> &paths){
-  if (input == "~")
-    chdir(getenv("HOME"));
-  else if (fs::exists(input))
-    chdir(input.c_str());
-  else std::cout << "cd: " << input << ": No such file or directory" << std::endl;
+  ifstream file(path);
+  if(file.good()){
+    string temp="";
+    for(int i=1;i<args.size();i++){
+      temp+=args[i]+" ";
+    }
+    string command="exec "+path+" "+temp;
+    system(command.c_str());
+  }
+  return true;
 }
 int main() {
+  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  std::pair<string, string> pairs;
-  std::map<string, void(*)(string input, vector<string> &path)> builtins = {
-    {"echo", echo},
-    {"type", type},
-    {"pwd", pwd},
-    {"cd", cd}
-  };
-  const string path = getenv("PATH");
-  //std::cout << path << std::endl;
-  vector<string> paths = splitWith(path, ':');
-  int status;
-  while (true){
+  // Uncomment this block to pass the first stage
+  while(true){
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    if (input == "exit 0")
+    stringstream ss(input);
+    string t;
+    char del=' ';
+    vector<string> args;
+    while(getline(ss,t,del)){
+      args.push_back(t);
+    }
+    if(input == "exit 0"){
       return 0;
-    pairs = split_first(input);
-    status = commandStatus(pairs.first, paths);
-    if (status == 0){
-      std::cout << input << ": command not found" << std::endl;
-    }else {
-      if (status == 1)
-        builtins[pairs.first](pairs.second, paths);
-      else {
-        std::system((paths[status-2] + "/" + pairs.first + " " + pairs.second).c_str());
+    }
+    else if(args[0]=="echo"){
+      if(input[5]=='\''){
+        cout<<input.substr(6,input.length()-7)<<endl;
       }
+      else if(input[5]=='\"'){
+        cout<<input.substr(6,input.length()-7)<<endl;
+      }
+      else{
+        for(int i=1;i<args.size();i++){
+          if(!args[i].empty()) cout<<args[i]<<" ";
+        }cout<<endl;
+      }
+    }
+    else if(input.find("type ")==0){
+      if(input.find("echo")!=string::npos or input.find("exit")!=string::npos or input.substr(5).find("type")!=string::npos or input.substr(5).find("pwd")!=string::npos){
+        cout<<input.substr(5)<<" is a shell builtin\n";
+      }
+      else{
+        string path = getPath(input.substr(5));
+        if(!path.empty()){
+          cout<<input.substr(5)<<" is "<<path<<endl;
+        }
+        else{
+          cout<<input.substr(5)<<": not found\n";
+        }
+      }
+    }
+    else if(args[0]=="pwd"){
+      string currPath=filesystem::current_path().string();
+      currPath=currPath.substr(0,currPath.length());
+      cout<<currPath<<endl;
+    }
+    else if(args[0]=="cd"){
+      if(args[1]=="~"){
+        chdir(getenv("HOME"));
+      }
+      else if(filesystem::exists(args[1])){
+        filesystem::current_path(args[1]);
+      }else{
+        cout<<"cd: "<<args[1]<<": No such file or directory\n";
+      }
+    }
+    else if(args[0]=="cat"){
+      // string ans;
+      system(input.c_str());
+    }
+    else{
+      if(!execprog(input)){
+        cout<<input<<": command not found\n";
+      }
+      // else cout<<input<<": command not found\n";
     }
   }
 }
