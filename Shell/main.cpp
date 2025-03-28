78f3a16080081b64cce6442540a79af18963db02
pmschz <paulinasanchez177@gmail.com>
2025-03-28 02:04:50 -0500

    added pwd

--- Shell/main.cpp
+++ Shell/main.cpp
@@ -1,73 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <sstream>
-#include <cstdlib>
-#include <fstream>
 #include <filesystem>
-std::vector<std::string> splitString(std::string string, char delimiter){
-  std::stringstream ss(string);
-  std::vector<std::string> return_vector;
-  std::string token;
-  while(getline(ss, token, delimiter)){
-    return_vector.push_back(token);
+#include <unistd.h>
+#include <sys/wait.h>
+std::string get_path(std::string command){
+  std::string path_env = std::getenv("PATH");
+  std::stringstream ss(path_env);
+  std::string path;
+  while(!ss.eof()){
+    getline(ss, path, ':');
+    std::string abs_path = path + "/" + command;
+    if (std::filesystem::exists(abs_path)) {
+        return abs_path;
+    }
   }
-  return return_vector;
-}
-void handleTypeCommand(std::string command, std::string arguments, std::vector<std::string> path_vect){
-  if(arguments == "exit" || arguments == "echo" || arguments == "type"){
-        std::cout << arguments << " is a shell builtin\n";
-      } else{
-        std::string filepath;
-        for(int i = 0; i < path_vect.size(); i++){
-          filepath = path_vect[i] + '/' + arguments;
-          std::ifstream file(filepath);
-          if(file.good()){
-            std::cout << arguments << " is " << filepath << "\n";
-            return;
-          }
-        }
-        std::cout << arguments << ": not found\n";
-      }
+  return "";
 }
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  std::string path_string = std::getenv("PATH");
-  std::vector<std::string> path_vector = splitString(path_string, ':');
   // Uncomment this block to pass the first stage
-  while(true){
-    std::cout << "$ ";
+  while(1){
+  std::cout << "$ ";
   
-    std::string input;
-    std::getline(std::cin, input);
-    if(input == "exit 0"){
-      return 0;
-    }
-    std::string command = input.substr(0, input.find(" "));
-    std::string arguments = input.substr(input.find(" ") + 1);
-    if(command == "echo"){
-      std:: cout << arguments << "\n";
-    }else if(command == "type"){
-      handleTypeCommand(command, arguments, path_vector);
-    } else if(command == "pwd"){
-      std::string cwd = std::filesystem::current_path();
-      std::string print_cwd = cwd.substr(0, cwd.length());
-      std::cout << print_cwd << "\n";
-    }else{
-      std::string filepath;
-      for(int i = 0; i < path_vector.size(); i++){
-        filepath = path_vector[i] + '/' + command;
-        std::ifstream file(filepath);
-        if(file.good()){
-          std::string exec_command = "exec " + path_vector[i] + "/" + input;
-          std::system(exec_command.c_str());
-          break;
+  std::string input;
+  std::getline(std::cin, input);
+  int end = input.find(" ");
+  std::string command = input.substr(0,end);
+  std::string arguments = input.substr(end+1);
+  if (command.compare("exit")==0){
+    return 0;
+    break;
+  }
+  else if (command.compare("echo")==0){
+    
+      std::cout<<arguments<<std::endl;
+  }
+  else if (command.compare("type")==0){
+      if(arguments.compare("echo") ==0 || arguments.compare("type") ==0 || arguments.compare("exit")==0)
+        std::cout<<arguments<<" is a shell builtin"<<std::endl;
+      else
+        {
+          std::string path = get_path(arguments);
+          if(path.empty())
+            std::cout<<arguments<<": not found\n";
+          else
+            std::cout<<arguments<<" is "<<path<<std::endl;
         }
-        else if(i == path_vector.size() - 1){
-          std::cout << command << ": not found\n";
-        } 
-      }
+  }
+  else if (command.compare("pwd")==0){
+    std::cout<<std::filesystem::current_path().string()<<std::endl;
+  }
+  else if (command.compare("cd")==0){
+    std::string absolute_path;
+    const std::string &path = arguments;
+    if (path[0] == '/')
+		{absolute_path = path;
+    
+	if (absolute_path.empty())
+		return 0;
+	if (chdir(absolute_path.c_str()) == -1)
+		std::cout << "cd: " << path << ": No such file or directory" << std::endl;
+    }
+    else if (path[0]== '.'){
+    std::string cwd = std::filesystem::current_path().string();
+    std::string dir = cwd + '/' + path;
+    cwd = std::filesystem::canonical(dir);
+    if (chdir(cwd.c_str()) == -1)
+		std::cout << "cd: " << path << ": No such file or directory" << std::endl;
+    }
+  }
+  else{
+    std::string path = get_path(command);
+    if(path.empty())
+            std::cout<< command <<": command not found"<<std::endl;
+          else
+            {
+              std::string command = path + " " + arguments;
+              int result = system(command.c_str());
+              if (result == -1) {
+              std::cerr << "Error executing the command" << std::endl;
+            }
+            }
     }
   }
 }
